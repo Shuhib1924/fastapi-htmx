731e7b649115aff105dca6089bb2b1e635e128e4
Shuhib <shuhib_s@live.de>
2025-04-22 12:54:06 +0200

    create & get all

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+todos = []
+
+
+def get_id():
+    if todos:
+        return todos[-1]["id"] + 1
+    return 1
+
+
+@app.post("/create")
+async def create_item(name: str):
+    todos.append({"id": get_id(), "name": name})
+    return todos
+
 
 @app.get("/")
 async def get_all():
-    return {"message": "Hello World"}
+    return todos
