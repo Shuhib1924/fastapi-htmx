a0557091258d668eb802ff26186504bb052391a2
Shuhib <shuhib_s@live.de>
2025-04-23 05:19:09 +0200

    refresh

--- main.py
+++ main.py
@@ -3,47 +3,47 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-todos = []
+# todos = [{'id': 1, 'name': 'Learn FastAPI'}, {'id': 2, 'name': 'Build an API'}]
 
 
-def get_id():
-    if todos:
-        return todos[-1]["id"] + 1
-    return 1
+# def get_id():
+#     if todos:
+#         return todos[-1]["id"] + 1
+#     return 1
 
 
-@app.post("/create")
-async def create_item(name: str):
-    todos.append({"id": get_id(), "name": name})
-    return todos
+# @app.post("/create")
+# async def create_item(name: str):
+#     todos.append({"id": get_id(), "name": name})
+#     return todos
 
 
-@app.get("/")
-async def get_all():
-    return todos
+# @app.get("/")
+# async def get_all():
+#     return todos
 
 
-@app.get("/single/{id}")
-async def get_single(id: int):
-    for todo in todos:
-        if todo["id"] == id:
-            return todo
-    return JSONResponse(status_code=404, content={"message": "Item not found"})
+# @app.get("/single/{id}")
+# async def get_single(id: int):
+#     for todo in todos:
+#         if todo["id"] == id:
+#             return todo
+#     return JSONResponse(status_code=404, content={"message": "Item not found"})
 
 
-@app.put("/update/{id}")
-async def update_item(id: int, name: str):
-    for todo in todos:
-        if todo["id"] == id:
-            todo["name"] = name
-            return todo
-    return JSONResponse(status_code=404, content={"message": "Item not found"})
+# @app.put("/update/{id}")
+# async def update_item(id: int, name: str):
+#     for todo in todos:
+#         if todo["id"] == id:
+#             todo["name"] = name
+#             return todo
+#     return JSONResponse(status_code=404, content={"message": "Item not found"})
 
 
-@app.delete("/delete/{id}")
-async def delete_item(id: int):
-    for todo in todos:
-        if todo["id"] == id:
-            todos.remove(todo)
-            return JSONResponse(status_code=201, content={"message": "Item deleted"})
-    return JSONResponse(status_code=404, content={"message": "Item not found"})
+# @app.delete("/delete/{id}")
+# async def delete_item(id: int):
+#     for todo in todos:
+#         if todo["id"] == id:
+#             todos.remove(todo)
+#             return JSONResponse(status_code=201, content={"message": "Item deleted"})
+#     return JSONResponse(status_code=404, content={"message": "Item not found"})
