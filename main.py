4254b983fbdb8cdda68c6d8ef2aab15ff36c4241
Shuhib <shuhib_s@live.de>
2025-04-25 08:20:13 +0200

    create and get all tasks

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import Depends, FastAPI, Form, Request, status
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
 from sqlmodel import Field, Session, SQLModel, create_engine
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 class Todo(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True, index=True)
     name: str
     completed: bool = Field(default=False)
     priority: int = Field(default=0)
@@ -18,9 +20,44 @@ engine = create_engine(
 SQLModel.metadata.create_all(engine)
 
 
+# def get_id(session: Session):
+#     count = session.query(Todo).count()
+#     return count + 1
+
+
 def get_session():
     with Session(engine) as session:
+        # new_id = get_id(session)
+        # print("id", new_id)
         yield session
+
+
+@app.post("/create", response_class=HTMLResponse, status_code=201)
+async def create_todo(request: Request, session: Session = Depends(get_session)):
+    todo_data = dict(await request.form())
+    # todo_data["id"] = get_id(session)
+    # print("data after", todo_data)
+    obj = Todo(**todo_data)
+    session.add(obj)
+    session.commit()
+    session.refresh(obj)
+    todo = session.query(Todo).get(obj.id)
+    # print("db:", todo)
+    return templates.TemplateResponse("task.html", {"request": request, "todo": todo})
+
+
+@app.get("/", response_class=HTMLResponse, status_code=200)
+async def get_all_tasks(request: Request, session: Session = Depends(get_session)):
+    todos = session.query(Todo).all()
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "todos": todos}
+    )
+
+
+# @app.post("/create", response_class=HTMLResponse, status_code=201)
+# async def create(request: Request, session: Session = Depends(get_session)):
+#     print("Received new task:", dict(await request.form()))
+#     return HTMLResponse(status_code=201, content="Task created successfully")
 
 
 # todos = [{'id': 1, 'name': 'Learn FastAPI'}, {'id': 2, 'name': 'Build an API'}]
